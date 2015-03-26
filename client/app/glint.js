960b647150561eb09cc57d91384d5ded8ea6c4e4
Jason Chang <jasonjchang@icloud.com>
2015-03-25 17:13:59 -0700

    Updated Minified with Fixed Merge Conflicts

--- client/app/glint.js
+++ client/app/glint.js
@@ -30,15 +30,11 @@ var app = angular.module('glint', [
         templateUrl: 'app/auth/signup.html',
         controller: "AuthCtrl as actrl"
       })
-<<<<<<< HEAD
-    .when('/ideas/:_id', {
-=======
     .when('/ideas/:_id/contributors', {
         templateUrl: 'app/ideaDetail/ideaDetail.html',
         controller: "IdeaContributorsCtrl as icbctrl"
       })
     .when('/ideas/:_id/comments', {
->>>>>>> Create separate folder for idea detail page
         templateUrl: 'app/ideas/ideaDetail.html',
         controller: "AuthCtrl as actrl"
       })
